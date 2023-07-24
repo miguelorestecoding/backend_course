2fbaab290ea36c25212c1204deaea1eb77de72f0
miguelorestecoding <miguelorestecoding@gmail.com>
2023-07-24 10:26:14 -0300

    Desafío Entregable 03 - Clase 06 - Funcionando.

--- DEC06_V00/app.js
+++ DEC06_V00/app.js
@@ -4,31 +4,46 @@
 // Se deberá utilizar la clase ProductManager que actualmente utilizamos con persistencia de archivos.
 // Desarrollar un servidor express que, en su archivo app.js importe al archivo de ProductManager que actualmente tenemos.
 import express from "express";
-import productManager from "./ProductManager.js"
+import productManager from "./ProductManager.js";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(8080, () => {
-    console.log("Escuchando puerto 8080")
-})
+  console.log("Escuchando puerto 8080");
+});
 
 // El servidor debe contar con los siguientes endpoints:
 // ruta ‘/products’, la cual debe leer el archivo de productos y devolverlos dentro de un objeto. Agregar el soporte para recibir por query param el valor ?limit= el cual recibirá un límite de resultados.
 // Si no se recibe query de límite, se devolverán todos los productos
 // Si se recibe un límite, sólo devolver el número de productos solicitados
 
-app.get('/api/products', async (req, res) => {
-    try {
-    const products = await productManager.getProducts()
-    res.status(200).json({messege: "Products", products })
-    } catch (err) {
-res.status(500).json({err})
+app.get("/api/products", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit);
+    let products = await productManager.getProducts();
+
+    if (!isNaN(limit) && limit > 0) {
+      products = products.slice(0, limit);
     }
+    res.status(200).json({ messege: "Products", products });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
 });
 
 // ruta ‘/products/:pid’, la cual debe recibir por req.params el pid (product Id), y devolver sólo el producto solicitado, en lugar de todos los productos.
+
+app.get("/api/products/:pid", async (req, res) => {
+  const { pid } = req.params;
+  try {
+    const product = await productManager.getProductsById(+pid);
+    res.status(200).json({ messege: "Product", product });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
 
 // Sugerencias
 // Tu clase lee archivos con promesas. recuerda usar async/await en tus endpoints
