d41a0a9da2d9b3afdd0f04cf07ef43f93fd80cf8
miguelorestecoding <miguelorestecoding@gmail.com>
2023-07-24 01:30:50 -0300

    get de products funcionando

---
+++ DEC06_V00/ProductManager.js
@@ -0,0 +1,147 @@
+import fs from 'fs';
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const infoProducts = await fs.promises.readFile(this.path, "utf8");
+        return JSON.parse(infoProducts);
+      } else {
+        return [];
+      }
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async addProduct(obj) {
+    try {
+      const productsPrev = await this.getProducts();
+      let id;
+      if (!productsPrev.length) {
+        id = 1;
+      } else {
+        id = productsPrev[productsPrev.length - 1].id + 1;
+      }
+      productsPrev.push({ ...obj, id });
+      await fs.promises.writeFile(this.path, JSON.stringify(productsPrev));
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async getProductsById(id) {
+    try {
+      const productsPrev = await this.getProducts();
+      const product = productsPrev.find((p) => p.id === id);
+      if (!product) {
+        return console.log("Imposible Mostrar: Ese producto no se encuentra en el listado.");
+      } else {
+        return product;
+      }
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      const productsPrev = await this.getProducts();
+      const product = productsPrev.find((p) => p.id === id);
+      if (!product) {
+        return console.log(
+          "Imposible Borrar: Ese producto no se encuentra en el listado."
+        );
+      } else {
+        const newProducts = productsPrev.filter((p) => p.id !== id);
+        await fs.promises.writeFile(this.path, JSON.stringify(newProducts));
+      }
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async updateProductById(id, obj) {
+    try {
+      const productsPrev = await this.getProducts(id);
+      const productIndex = productsPrev.findIndex((p) => p.id === id);
+      if (productIndex === -1) {
+        return console.log(
+          "Imposible Actualizar: Ese producto no se encuentra en el listado."
+        );
+      } else if ('id' in obj) {
+        return console.log("Imposible Actualizar: No se permite sobrescribir el campo id");
+      } else {
+        const productToUpdate = productsPrev[productIndex];
+        productsPrev[productIndex] = {...productToUpdate, ...obj}
+        await fs.promises.writeFile(this.path, JSON.stringify(productsPrev))
+      }      
+    } catch (err) {return err;}
+  }
+}
+
+const productUpdated = {
+  title: "productUpdated",
+  age: 100,
+}
+
+const productIdUpdated = {
+  title: "productUpdated",
+  id: 100,
+}
+
+const testProduct = {
+  title: "producto prueba",
+  description: "Este es un producto prueba",
+  price: 200,
+  thumbnail: "Sin Imagen",
+  code: "abc123",
+  stock: 25,
+};
+
+async function testing() {
+  //Se creará una instancia de la clase “ProductManager”
+  const myProductManager = new ProductManager("Products.json");
+
+  // Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+  let products = await myProductManager.getProducts()
+  console.log(products);
+
+// Se llamará al método “addProduct” con los campos:
+// title: “producto prueba”
+// description:”Este es un producto prueba”
+// price:200,
+// thumbnail:”Sin imagen”
+// code:”abc123”,
+// stock:25
+// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+  await myProductManager.addProduct(testProduct);
+  await myProductManager.addProduct(testProduct);
+
+  // Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+  products = await myProductManager.getProducts()
+  console.log(products);
+
+  // Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.
+  console.log(await myProductManager.getProductsById(1));
+  console.log(await myProductManager.getProductsById(3));
+
+  // Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.
+  console.log(await myProductManager.updateProductById(1, productIdUpdated))
+  await myProductManager.updateProductById(1, productUpdated)
+  console.log(await myProductManager.getProductsById(1));
+
+  // Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.
+  await myProductManager.deleteProduct(2)
+  console.log(await myProductManager.getProducts());
+  console.log(await myProductManager.deleteProduct(3));  
+}
+
+// testing();
+
+const  productManager = new ProductManager('Products.json');
+export default productManager;
--- DEC06_V00/app.js
+++ DEC06_V00/app.js
@@ -4,25 +4,29 @@
 // Se deberá utilizar la clase ProductManager que actualmente utilizamos con persistencia de archivos.
 // Desarrollar un servidor express que, en su archivo app.js importe al archivo de ProductManager que actualmente tenemos.
 import express from "express";
-import ProductManager from "../DEC04_V00/ProductManager"
+import productManager from "./ProductManager.js"
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.get('/api/products', async (req, res) => {
-    try {
-    const products = await ProductManager.getProducts()
-    res.status(200).json({messege: "Products", products })
-    } catch (err) {
-res.status(500).json({err})
-    }
-});
+app.listen(8080, () => {
+    console.log("Escuchando puerto 8080")
+})
 
 // El servidor debe contar con los siguientes endpoints:
 // ruta ‘/products’, la cual debe leer el archivo de productos y devolverlos dentro de un objeto. Agregar el soporte para recibir por query param el valor ?limit= el cual recibirá un límite de resultados.
 // Si no se recibe query de límite, se devolverán todos los productos
 // Si se recibe un límite, sólo devolver el número de productos solicitados
+
+app.get('/api/products', async (req, res) => {
+    try {
+    const products = await productManager.getProducts()
+    res.status(200).json({messege: "Products", products })
+    } catch (err) {
+res.status(500).json({err})
+    }
+});
 
 // ruta ‘/products/:pid’, la cual debe recibir por req.params el pid (product Id), y devolver sólo el producto solicitado, en lugar de todos los productos.
 
