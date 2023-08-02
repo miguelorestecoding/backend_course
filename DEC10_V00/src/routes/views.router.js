4b9630095edee3c94d140f3181660f570d0cd9e2
miguelorestecoding <miguelorestecoding@gmail.com>
2023-08-02 18:45:37 -0300

    handlebars routes workin. Metodos desde handlebars funcionando. Falta agregar el redireccionar.

--- DEC10_V00/src/routes/views.router.js
+++ DEC10_V00/src/routes/views.router.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import productManager from "../ProductManager.js";
+
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -7,6 +9,12 @@ router.get('/', (req, res) => {
 
 router.get('/addProduct', (req, res) => {
 res.render('addProduct')
+});
+
+router.get('/products', async (req, res) => {
+    const products = await productManager.getProducts()
+    console.log(products)
+    res.render('products', {products})
 });
 
 export default router
