854518b154315b9a62bd1ca320af79f33f895435
Miguel Oreste Capó <105470239+miguelorestecoding@users.noreply.github.com>
2023-08-07 10:47:43 -0300

    Trabajando en el realTimeProducts. Botón add funcionando. Intentando actualización con WebSockets

--- DEC10_V00/src/app.js
+++ DEC10_V00/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
 import viewsRouter from './routes/views.router.js';
+import fs from "fs";
 import  { __dirname } from './utils.js';
 import { engine } from 'express-handlebars';
 import { Server } from 'socket.io';
@@ -23,9 +24,20 @@ app.use('/api/carts/', cartRouter);
 
 app.use('/', viewsRouter);
 
+//ProductManager
+async function readProductsJson() {
+  try {
+    const productsData = await fs.promises.readFile(__dirname+'/Products.json', "utf8");
+    const realTimeProducts = JSON.parse(productsData);
+    console.log(realTimeProducts);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
 const PORT = 3000
 
+// Websocket
 const httpServer = app.listen(PORT, () => {
     console.log(`Escuchando el puerto ${PORT}` )
 });
@@ -33,5 +45,15 @@ const httpServer = app.listen(PORT, () => {
 const socketServer = new Server(httpServer)
 
 socketServer.on('connection', (socket) => {
-    console.log(`Escucho que el cliente con id: ${socket.id} ha emitido un evento 'connection'`)
+    console.log(`Escucho que el cliente con id: ${socket.id} ha emitido un evento 'connection'`);
+
+    socket.on('disconnect', () => {
+        console.log(`Escucho que un cliente ha emitido un evento 'disconnect'`)
+    })
+
+    socket.emit('bienvenida', `Soy el servidor y te doy la bienvenida, querido usuario ${socket.id} !` )
+
+    socket.on('productAdded', (message) => {
+        console.log(`Escucho que el cliente me dice: ${message}`)
+    })
 })
--- DEC10_V00/src/public/js/index.js
+++ DEC10_V00/src/public/js/index.js
@@ -1,1 +1,8 @@
-const socketClient = io()
+const socketClient = io();
+
+const realTimeProductsForm = document.getElementById("realTimeProductsForm");
+
+socketClient.on('bienvenida', (message) => {
+console.log(`He recibo un evento del Servidor que dice: ${message}`);
+})
+
