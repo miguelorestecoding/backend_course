71ca02a0f9ba87512178bb5733ff50776d7534a6
Miguel Oreste Capó <105470239+miguelorestecoding@users.noreply.github.com>
2023-08-08 14:44:38 -0300

    Entregable. Arreglo renderizaciión de todas las propiedades de los productos.

--- DEC10_V00/src/public/js/index.js
+++ DEC10_V00/src/public/js/index.js
@@ -6,7 +6,16 @@ socketClient.on("productos-actualizados", (products) => {
   // Codigo que renderice dentro del <div id="divRealTimeProducts"></div> que se encuentra en el archivo realTimeProducts.handlebars el detalle de todos los productos que llegan en "productos"
   const realTimeProducts = products
   .map((product) => {
-    return `<p>id : ${product.id}</p> <p>Title: ${product.title}</p>`;
+    return `
+    <p>id : ${product.id}</p> 
+    <p>Title: ${product.title}</p>
+    <p>Description: ${product.description}</p>
+    <p>Code: ${product.code}</p>
+    <p>Price: ${product.price}</p>
+    <p>Category: ${product.category}</p>
+    <p>Thumbnails: ${product.thumbnails}</p>
+    <hr>
+    `;
   })
   .join('');
   divRealTimeProducts.innerHTML = realTimeProducts
